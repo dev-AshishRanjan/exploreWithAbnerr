965e5f12ed71d340485e91a7bdfadfb5e79c6707
dev-AshishRanjan <94990764+dev-AshishRanjan@users.noreply.github.com>
2023-11-04 14:07:41 +0530

    ADD : Contact section

--- src/pages/admin.jsx
+++ src/pages/admin.jsx
@@ -1,13 +1,11 @@
 import Head from "next/head";
 import styles from "@/styles/Home.module.scss";
 import DummyPage from "@/style-guide/page-components/DummyPage";
-import ContactSection from "@/style-guide/components/ContactSection";
 
 export default function Home() {
   return (
     <>
       <DummyPage />
-      <ContactSection />
     </>
   );
 }
