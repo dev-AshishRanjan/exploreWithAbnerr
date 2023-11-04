05ca95b545f00863b5521bebf4a20c290981653b
dev-AshishRanjan <94990764+dev-AshishRanjan@users.noreply.github.com>
2023-11-04 14:41:47 +0530

    ADD : Admin Panel Init

--- src/style-guide/components/AdminPanel/index.jsx
+++ src/style-guide/components/AdminPanel/index.jsx
@@ -2,81 +2,9 @@ import React, { useState, useEffect } from "react";
 import styles from "./style.module.scss";
 
 const AdminPanel = () => {
-  const [portraits, setPortraits] = useState();
-  const [adventure, setAdventure] = useState();
-  useEffect(() => {
-    fetch("/gallery/portraits")
-      .then((req) => req.json())
-      .then((res) => {
-        setPortraits(JSON.stringify(res.data));
-      });
-    fetch("/gallery/adventure")
-      .then((req) => req.json())
-      .then((res) => {
-        setAdventure(JSON.stringify(res.data));
-      });
-  }, []);
-  const handlePortraitSubmit = (e) => {
-    e.preventDefault();
-    fetch(`/gallery/portraits`, {
-      method: "POST",
-      body: JSON.stringify({ portraits }),
-    })
-      .then((req) => req.json())
-      .then((res) => {
-        console.log({ res });
-        if (res.status === 200) {
-          alert("Success!");
-        }
-      });
-  };
-  const handleAdventureSubmit = (e) => {
-    e.preventDefault();
-    fetch(`/gallery/adventure`, {
-      method: "POST",
-      body: JSON.stringify({ adventure }),
-    })
-      .then((req) => req.json())
-      .then((res) => {
-        console.log({ res });
-        if (res.status === 200) {
-          alert("Success!");
-        }
-      });
-  };
+  // codes form admin console
 
-  return (
-    <div className={styles.AdminPanel}>
-      <div className={styles.group}>
-        <h2>Portraits</h2>
-        <textarea
-          name="skills"
-          id="skills"
-          cols="30"
-          rows="10"
-          value={skills}
-          onChange={(e) => setPortraits(e.target.value)}
-        ></textarea>
-        <button type="submit" className="btn" onClick={handlePortraitSubmit}>
-          submit
-        </button>
-      </div>
-      <div className={styles.group}>
-        <h2>Adventure</h2>
-        <textarea
-          name="projects"
-          id="projects"
-          cols="30"
-          rows="20"
-          value={project}
-          onChange={(e) => setAdventure(e.target.value)}
-        ></textarea>
-        <button type="submit" className="btn" onClick={handleAdventureSubmit}>
-          submit
-        </button>
-      </div>
-    </div>
-  );
+  return <div className={styles.AdminPanel}>{/* admin console form */}</div>;
 };
 
 export default AdminPanel;
