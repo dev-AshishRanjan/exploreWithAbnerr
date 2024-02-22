541ccf4bbbf3ae503309616cb927ae37bbb79ab5
Tolemy Kashyap <kashyaptolemy@gmail.com>
2024-02-22 19:01:43 +0530

    fixes

--- src/pages/goldenhour.jsx
+++ src/pages/goldenhour.jsx
@@ -4,7 +4,20 @@ import GalleryGrid from "../style-guide/components/GalleryGrid";
 const goldenhour = () => {
   return (
     <>
-      <GalleryGrid />
+      <GalleryGrid
+        images={[
+          "/images/winter/1.jpg",
+          "/images/winter/2.jpg",
+          "/images/winter/3.jpg",
+          "/images/winter/4.jpg",
+          "/images/winter/5.jpg",
+          "/images/winter/6.jpg",
+          "/images/winter/7.jpg",
+          "/images/winter/8.jpg",
+          "/images/winter/9.jpg",
+          "/images/winter/10.jpg",
+        ]}
+      />
     </>
   );
 };
